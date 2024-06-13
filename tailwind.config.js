8b0ef7dea6fb95cecf02f605f72244d7f735c9e0
shamim150 <161612149+shamim150@users.noreply.github.com>
2024-06-13 15:34:21 +0600

    nav-heros-section

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container:{
+       screens:{
+        DEFAULT: '100%',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px',
+       }
+      }
+    }
+    },
+
+  plugins: [],
+}
