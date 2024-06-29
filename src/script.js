c781d1fa63e56b55be4055f03a410951c288ac10
shamim150 <161612149+shamim150@users.noreply.github.com>
2024-06-29 16:02:39 +0600

    footer

---
+++ src/script.js
@@ -0,0 +1,47 @@
+let head = document.querySelector(".head")
+let head2 = document.querySelector(".head2")
+let head3 = document.querySelector(".head3")
+let head4 = document.querySelector(".head4")
+let head5 = document.querySelector(".head5")
+let head6 = document.querySelector(".head6")
+
+
+let click = document.querySelector(".click")
+let click2 = document.querySelector(".click2")
+let click3 = document.querySelector(".click3")
+let click4 = document.querySelector(".click4")
+let click5 = document.querySelector(".click5")
+let click6 = document.querySelector(".click6")
+
+
+
+
+
+
+head.addEventListener("click", function(){
+    click.classList.toggle('show')
+
+})
+
+head2.addEventListener("click", function(){
+    click2.classList.toggle('show')
+
+})
+
+
+head3.addEventListener("click", function(){
+    click3.classList.toggle('show')
+
+})
+head4.addEventListener("click", function(){
+    click4.classList.toggle('show')
+
+})
+head5.addEventListener("click", function(){
+    click5.classList.toggle('show')
+
+})
+head6.addEventListener("click", function(){
+    click6.classList.toggle('show')
+
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,47 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      container:{
-       screens:{
-        DEFAULT: '100%',
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
-        '3xl': '1920px',
-       }
-      }
-    }
+
+
+
+
+    fontFamily: {
+      'cormo': ['Cormorant Upright', 'serif',]
+  
     },
 
+
+
+
+
+    extend: {
+      animation: {
+        "slow-spin": "spin 10s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+
+      container: {
+        screens: {
+          DEFAULT: "100%",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+          "3xl": "1920px",
+        },
+      },
+    },
+  },
+
   plugins: [],
-}
+};
